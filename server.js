debfc794ae18cba8196677f5d6bb75d51c18d00a
benchatfield42 <bchatfi1@asu.edu>
2024-11-02 18:20:41 -0700

    Interpret data

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
+    if (req.method === 'GET' && req.url.startsWith('/sendData')) {
+        // Extract query parameters
+        const queryObject = url.parse(req.url, true).query;
+
+        const x = queryObject.x;
+        const y = queryObject.y;
+        const z = queryObject.z;
+        const direction = queryObject.direction;
+
+        // Here you can process the data as needed, e.g., log it or use it for your application
+        console.log(`Received data: X=${x}, Y=${y}, Z=${z}, Direction=${direction}`);
+
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Data received successfully');
+    } else if (req.url === '/') {
         fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
             if (err) {
                 res.writeHead(500);
